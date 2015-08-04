76190156b1b73df56cba94dae29587d645c63a61
rccc <rccc@diogene>
2015-08-04 14:23:33 +0200

    namespace typo

--- Command/GenerateCommand.php
+++ Command/GenerateCommand.php
@@ -7,7 +7,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use BHW\Bundle\CoreBundle\Generator\SonataAdminTestsGenerator;
+use BVM\SonataAdminTestsGeneratorBundle\Generator\SonataAdminTestsGenerator;
 use Doctrine\Common\Annotations\AnnotationReader;
 
 
